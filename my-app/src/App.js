fabfa69407dfb156f66715660126e4099f0aa2c3
s1f102103707 <s1f102103707@iniad.org>
2023-10-30 14:16:11 +0900

    button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,10 +3,14 @@ import Message from "./Message";
 import Curriculm from "./Curriculm";
 
 function App() {
+  const OnClick = () => {
+    alert("ボタンが押されました");
+  };
   return (
     <div>
       <Message />
       <Curriculm />
+      <button onClick={OnClick}>Click</button>
     </div>
   );
 }
