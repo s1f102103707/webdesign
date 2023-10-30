f192f6a7ad4406121ace11cc8ab03bd05b91167b
s1f102103707 <s1f102103707@iniad.org>
2023-10-30 13:56:03 +0900

    set

---
+++ my-app/src/Curriculm.jsx
@@ -0,0 +1,4 @@
+const Curriculm = () => {
+  return <p>Computer science is important for all.</p>;
+};
+export default Curriculm;
---
+++ my-app/src/Message.jsx
@@ -0,0 +1,9 @@
+const Message = () => {
+  return (
+    <div>
+      <h2>welcome to INIAD!</h2>
+      <p>INIAD is ...</p>
+    </div>
+  );
+};
+export default Message;
